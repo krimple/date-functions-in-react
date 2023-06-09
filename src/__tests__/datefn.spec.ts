d3763bf40bb1713d5e99cd2eef86fed2a1e6f855
Ken Rimple <krimple@chariotsolutions.com>
2023-06-09 12:53:22 -0400

    updated to use luxon tests too

--- src/__tests__/datefn.spec.ts
+++ src/__tests__/datefn.spec.ts
@@ -1,15 +1,14 @@
 import {describe, it, expect} from 'vitest';
 import {subWeeks, endOfDay} from 'date-fns';
 import {zonedTimeToUtc} from 'date-fns-tz';
+import {DateTime} from 'luxon';
 
 describe('date fn tests', () => {
     it('zonedTimeToUTC time at 3/12/2023 at 1:40AM (before 2AM cutover) is in EST (+5)', () => {
         // EDT date that is in leap time
         // 240AM Eastern ny time
         const myDate = new Date(2023, 2, 12, 1, 40, 0, 0);
-        console.log(`date: ${myDate.toString()}`);
         const laterZonedTime= zonedTimeToUtc(myDate, 'America/New_York');
-        console.log(`utc date: ${laterZonedTime.toUTCString()}`);
         expect(laterZonedTime.getDate()).toBe(12);
         // should be 6AM (five hours ahead of 1AM)
         expect(laterZonedTime.getUTCHours()).toBe(6);
@@ -22,9 +21,7 @@ describe('date fn tests', () => {
 
     it('zonedTimeToUTC time at 3/12/2023 at 2:40AM (that lost 2AM hour) is in EST (+5)', () => {
         const myDate = new Date(2023, 2, 12, 2, 40, 0, 0);
-        console.log(`date: ${myDate.toString()}`);
         const laterZonedTime= zonedTimeToUtc(myDate, 'America/New_York');
-        console.log(`utc date: ${laterZonedTime.toUTCString()}`);
         expect(laterZonedTime.getDate()).toBe(12);
         // should be 7AM (fiver hours ahead of 2AM)
         expect(laterZonedTime.getUTCHours()).toBe(7);
@@ -67,4 +64,36 @@ describe('date fn tests', () => {
         // 4 hours later in UTC - we're between 2 and 3 AM EDT - is that right?
         expect(utcTimeData).toEqual('2023-03-12T07:01:00.000Z');
     });
-});
+
+    it("luxon dates - before DST switch", () => {
+        const myDate = new Date(2023, 2, 12, 0, 40, 0, 0);
+        const luxonDate = DateTime.fromJSDate(myDate);
+        expect(luxonDate.isValid).toBeTruthy();
+        // EST time...
+        expect(luxonDate.toISO()).toBe('2023-03-12T00:40:00.000-05:00');
+        // UTC time - 5 hrs later
+        expect(luxonDate.toUTC().toString()).toBe('2023-03-12T05:40:00.000Z');
+        expect(luxonDate.isValid).toBeTruthy();
+    });
+    it("luxon dates - dst dead zone - this is a non-real date", () => {
+        const myDate = new Date(2023, 2, 12, 2, 40, 0, 0);
+        const luxonDate = DateTime.fromJSDate(myDate);
+        // iso of luxon date using EDT in the deadzone - 4 hrs
+        expect(luxonDate.toISO()).toBe('2023-03-12T03:40:00.000-04:00');
+        // UTC: also 4 hours
+        expect(luxonDate.toUTC().toString()).toBe('2023-03-12T07:40:00.000Z');
+
+        // this is a logically invalid date but it passes so commented out
+        // expect(luxonDate.toUTC().isValid).toBeFalsy();
+    });
+
+    it("luxon dates - dst", () => {
+        const myDate = new Date(2023, 2, 12, 3, 40, 0, 0);
+        const luxonDate = DateTime.fromJSDate(myDate);
+        // iso of luxon date using EDT - 4 hrs
+        expect(luxonDate.toISO()).toBe('2023-03-12T03:40:00.000-04:00');
+        // UTC: also 4 hours
+        expect(luxonDate.toUTC().toString()).toBe('2023-03-12T07:40:00.000Z');
+
+        expect(luxonDate.isValid).toBeTruthy();
+    });});
